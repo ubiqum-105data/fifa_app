1e243064511913612e486b68767451d9d8c6b5a4
Tito Lins <tito@blinx.com.br>
2018-11-12 16:34:25 +0100

    import data is working (but do not create the table with `create_table.py`)

--- src/import_data.py
+++ src/import_data.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 import pandas as pd
-
+from sqlalchemy import create_engine
+from config.db import get_db_string
 
 if __name__ == '__main__':
-    df = pd.read_csv('../data/fifa2005.csv', encoding = 'utf-8')
+    df = pd.read_csv('../data/fifa2005.csv', encoding = 'utf-8',
+                     index_col = 'id')
     overall = df[['overall']]
     potential = [int(str(p[0])[2:]) for p in overall.values]
     overall = [int(str(p[0])[:2]) for p in overall.values]
     df['overall'] = overall
     df['potential'] = potential
+    engine = create_engine(get_db_string())
+    df.to_sql(name = 'player', con = engine, if_exists = 'append',
+              index_label = 'id')
+

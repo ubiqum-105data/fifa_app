0f47e07eabbc6f6822063bbd4ccb6713e25aa95f
Tito Lins <tito@blinx.com.br>
2018-11-12 17:57:54 +0100

    added config files support (loads `src/config/local.json` when present and `src/config/default.json` when `local.json` not present).

--- src/config/db.py
+++ src/config/db.py
@@ -1,14 +1,27 @@
-USER = 'postgres'
-PASSWORD = '1234'
-DRIVER = 'psycopg2'
-DB = 'fifa'
+import json
 
+LOCAL_CONFIG_FILE = 'config/local.json'
+DEFAULT_CONFIG_FILE = 'config/default.json'
+
+def get_config():
+    try:
+        with open(LOCAL_CONFIG_FILE) as json_data:
+            d = json.load(json_data)
+            return d
+    except FileNotFoundError:
+        with open(DEFAULT_CONFIG_FILE) as json_data:
+            d = json.load(json_data)
+            return d
+
+'''
 def config_db_string(app):
-    app.config['DB_STRING'] = "postgresql+%s://%s:%s@localhost/%s" \
-        % (DRIVER, USER, PASSWORD, DB,)
+    app.config['DB_STRING'] = "postgresql+%s://%s:%s@%s/%s" \
+        % (DRIVER, USER, PASSWORD, address, DB,)
+'''
 
 def get_db_string():
-    return "postgresql+%s://%s:%s@localhost/%s" \
-        % (DRIVER, USER, PASSWORD, DB,)
-
+    config = get_config()
+    return "postgresql+%s://%s:%s@%s/%s" \
+        % (config['driver'], config['user'], config['password'],
+           config['address'], config['db'],)
 

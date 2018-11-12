afdcf4061e1b93901f0b4e0bd82faa44fe234a64
Tito Lins <tito@blinx.com.br>
2018-11-12 17:29:17 +0100

    import now imports several files

--- src/import_data.py
+++ src/import_data.py
@@ -3,15 +3,26 @@ import pandas as pd
 from sqlalchemy import create_engine
 from config.db import get_db_string
 
+# bidimensional array containing 2-sized arrays, where a[0][0] = the relative
+# file path for the data to be imported and a[0][1] contains the name of the
+# table
+IMPORT_DATA = [
+    ['../data/fifa2005.csv', 'fifa_2005'],
+    ['../data/fifa2006.csv', 'fifa_2006'],
+]
+
 if __name__ == '__main__':
-    df = pd.read_csv('../data/fifa2005.csv', encoding = 'utf-8',
-                     index_col = 'id')
-    overall = df[['overall']]
-    potential = [int(str(p[0])[2:]) for p in overall.values]
-    overall = [int(str(p[0])[:2]) for p in overall.values]
-    df['overall'] = overall
-    df['potential'] = potential
-    engine = create_engine(get_db_string())
-    df.to_sql(name = 'player', con = engine, if_exists = 'append',
-              index_label = 'id')
+    for d in IMPORT_DATA:
+        df = pd.read_csv(d[0], encoding = 'utf-8',
+                         index_col = 'id')
+        if 'edition' in df:
+            df = df.drop(columns = ['edition'])
+        overall = df[['overall']]
+        potential = [int(str(p[0])[2:]) for p in overall.values]
+        overall = [int(str(p[0])[:2]) for p in overall.values]
+        df['overall'] = overall
+        df['potential'] = potential
+        engine = create_engine(get_db_string())
+        df.to_sql(name = d[1], con = engine, if_exists = 'append',
+                  index_label = 'id')
 
